7d6b5f5a9c1a51869f20aeea80db39051b38c70c
staks <stas.sor@gmail.com>
2022-01-30 13:11:21 +0300

    Не придумал ничего лучше как использовать данный метод.

--- main.py
+++ main.py
@@ -1,16 +1,54 @@
-# This is a sample Python script.
+"""Домашнее задание для курса "Основы Python". Переменные, условия и циклы"""
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import matplotlib.pyplot as plt
+import numpy as np
+from numpy import sqrt
 
+X = [0, 2, 5, 6, 8] # Координаты Х
+Y = [2, 5, 2, 6, 3] # Координаты Y
+n = len(X)
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+Road = []
+RW = []
+RIB = []
+s = []
 
+for ib in np.arange(0, n, 1):
+    M = np.zeros([n, n])
+    for i in np.arange(0, n, 1):
+        for j in np.arange(0, n, 1):
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+            if i != j:
+                M[i, j] = sqrt((X[i] - X[j]) ** 2 + (Y[i] - Y[j]) ** 2)
+            else:
+                M[i, j] = float('inf')
+    way = [ib]
+    for i in np.arange(1, n, 1):
+        s = []
+        for j in np.arange(0, n, 1):
+            s.append(M[way[i - 1], j])
+        way.append(s.index(min(s)))
+        for j in np.arange(0, i, 1):
+            M[way[i], way[j]] = float('inf')
+            M[way[i], way[j]] = float('inf')
+    S = sum([sqrt((X[way[i]] - X[way[i + 1]]) ** 2 + (Y[way[i]] - Y[way[i + 1]]) ** 2) for i in
+             np.arange(0, n - 1, 1)]) + sqrt((X[way[n - 1]] - X[way[0]]) ** 2 + (Y[way[n - 1]] - Y[way[0]]) ** 2)
+    Road.append(S)
+    RW.append(way)
+    RIB.append(ib)
+S = min(Road)
+way = RW[Road.index(min(Road))]
+ib = RIB[Road.index(min(Road))]
+X1 = [X[way[i]] for i in np.arange(0, n, 1)]
+Y1 = [Y[way[i]] for i in np.arange(0, n, 1)]
+#
+plt.title('Общий путь-%s.-%i-%i.\n Координаты X,Y заданы' % (round(S, 3), ib, n), size=14)
+plt.plot(X1, Y1, color='r', linestyle=' ', marker='o')
+plt.plot(X1, Y1, color='b', linewidth=1)
+X2 = [X[way[n - 1]], X[way[0]]]
+Y2 = [Y[way[n - 1]], Y[way[0]]]
+#
+plt.plot(X2, Y2, color='g', linewidth=2, linestyle='-', label='Путь от  последней точки \n к первой')
+plt.legend(loc='best')
+plt.grid(True)
+plt.show()
